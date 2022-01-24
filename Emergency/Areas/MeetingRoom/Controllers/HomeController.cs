53d6060d4a17250a6d58f9831eaf5ca59973dcbc
phanthaianths1975 <96382790+phanthaianths1975@users.noreply.github.com>
2022-01-24 14:24:41 +0700

    meeting room

---
+++ Emergency/Areas/MeetingRoom/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emergency.Areas.MeetingRoom.Controllers
+{
+    [Area("MeetingRoom")]//Declare Area
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignIn(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("MeetingRoom", "Scheduler", new { area = "MeetingRoom" });
+            }
+            //return RedirectToAction("error", "Home", new { area = "DarkSiteEmergency" });
+            return View();
+        }
+    }
+}
